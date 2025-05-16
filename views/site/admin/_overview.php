5e5cd291b8366a011a59ccd83db1e10c44459d11
kleanthis <kvichos@athenarc.gr>
2025-05-16 04:33:21 +0300

    Add registered users graph in admin page

--- views/site/admin/_overview.php
+++ views/site/admin/_overview.php
@@ -4,6 +4,10 @@ use Yii;
 use yii\helpers\Url;
 use yii\helpers\Html;
 
+echo Html::jsFile('@web/js/chartjs_bar_plot_admin.js?v=' . filemtime(Yii::getAlias('@webroot/js/chartjs_bar_plot_admin.js')));
+echo Html::jsFile('@web/js/third-party/chartjs/chart_v4.2.0.js');
+echo Html::jsFile('@web/js/third-party/chartjs/chart_labels_v2.2.0.js');
+echo Html::cssFile('@web/css/template/green-black.css');
 
 ?>
 
@@ -13,7 +17,13 @@ use yii\helpers\Html;
         <div class="panel panel-default">
             <div class="panel-heading">User Statistics</div>
                 <div class="panel-body">
-                <p><strong>Number of Users:</strong> <?= $stats->total_users ?></p>
+                    <p><strong>Number of Users:</strong> <?= $stats->total_users ?></p>
+                    <div style="position:relative; height:50%; width:50">
+                        <canvas id="registered_users-bar-plot"></canvas>
+                    </div>
+                    <script>
+                        render_admin_bar_plot('registered_users-bar-plot', <?= $graph_labels ?>, <?= $graph_data ?>, 'Registered users');
+                    </script>
                 </div>
             </div>
       </div>
