c88ab98bca3ba57aa674a2f2ba71082ecb16e54a
serafeim <s.chatzopoulos@gmail.com>
2025-06-20 16:31:06 +0300

    Change info for AI summary

--- views/site/index.php
+++ views/site/index.php
@@ -219,11 +219,26 @@ if ($in_space) {
                                     <div id="summaryContent" class="grey-text">
                                         <div class="summary-controls">
                                             <div id="regenerate-summary-box" class="regenerate-summary-box" style="display: none;">
-                                                <i class="fa fa-info-circle text-muted" title="Change the number of top results used for summarization."></i>
                                                 <label for="summary-count" class="regenerate-label">Use top</label>
                                                 <input type="number" id="summary-count" class="regenerate-input" />
                                                 <label for="summary-count" class="regenerate-label">results.</label>
-                                                <button id="regenerate-summary-btn" class="btn btn-sm btn-custom-color regenerate-button">Generate</button>
+                                                <span 
+                                                    role="button" 
+                                                    data-toggle="popover" 
+                                                    data-placement="auto" 
+                                                    title="AI Summary" 
+                                                    data-content="<p>The summary format will change based on the selected number of papers:</p>
+                                                    <ul>
+                                                        <li>1-5 papers: Produces a concise overview.</li>
+                                                        <li>6-20 papers: Creates a more detailed, literature review-style summary.</li>
+                                                    </ul>
+                                                    "
+                                                    style="cursor: pointer;"
+                                                > 
+                                                    <small><i class="fa fa-info-circle light-grey-link" aria-hidden="true"></i></small>
+                                                </span>
+
+                                                <button id="regenerate-summary-btn" class="btn btn-sm btn-custom-color regenerate-button">Summarize</button>
                                             </div>
                                             <div class="text-right" id="copy-summary-wrapper" style="display: none;">
                                                 <a id="copy-summary-btn" class="btn btn-default btn-xs fs-inherit grey-link" role="button" data-toggle="tooltip">
