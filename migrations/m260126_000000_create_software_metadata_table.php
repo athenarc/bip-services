8f8197a861880fa77eaf22ecc26cb30a00ae33f3
serafeim <s.chatzopoulos@gmail.com>
2026-01-28 15:42:06 +0200

    Update software metadata table migration

--- migrations/m260126_000000_create_software_metadata_table.php
+++ migrations/m260126_000000_create_software_metadata_table.php
@@ -5,13 +5,8 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%software_metadata}}`.
  */
-class m260126_000000_create_software_metadata_table extends Migration
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
+class m260126_000000_create_software_metadata_table extends Migration {
+    public function safeUp() {
         $this->createTable('{{%software_metadata}}', [
             'id' => $this->primaryKey(),
             'paper_id' => $this->integer()->notNull(),
@@ -23,15 +18,9 @@ class m260126_000000_create_software_metadata_table extends Migration
 
         // Indexes for faster queries
         $this->createIndex('idx_software_metadata_paper_id', '{{%software_metadata}}', 'paper_id');
-        $this->createIndex('idx_software_metadata_license', '{{%software_metadata}}', 'license');
-        $this->createIndex('idx_software_metadata_access_right', '{{%software_metadata}}', 'access_right');
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
+    public function safeDown() {
         $this->dropTable('{{%software_metadata}}');
     }
 }
