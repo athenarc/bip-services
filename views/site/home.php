8b22ad56ac0f99f0fa10fdc03f26f59c7dc8d455
serafeim <s.chatzopoulos@gmail.com>
2026-04-16 13:01:47 +0300

    Move home service cards into the jumbotron.
    Keep the homepage service links visually grouped with the main hero section by rendering the service card block inside the jumbotron container.

--- views/site/home.php
+++ views/site/home.php
@@ -13,8 +13,7 @@ $this->registerCssFile('@web/css/home.css');
     <div class="jumbotron">
         <h1><?= Html::img('@web/img/bip-minimal-10-years.png', ['class' => 'img-responsive center-block', 'width' => 200]) ?></h1>
         <p style = "margin-top:-10px;">Amplifying valuable research</p>
-    </div>
-    <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+        <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
             <?php foreach (Yii::$app->params['services'] as $service) { ?>
                 <a class="service-card btn btn-default btn-lg" role="button" href="<?= Url::to($service['url']) ?>">
                     <div class="service-header main-green">
@@ -26,5 +25,6 @@ $this->registerCssFile('@web/css/home.css');
                     </div>
                 </a>
             <?php } ?>
+        </div>
     </div>
 </div>
